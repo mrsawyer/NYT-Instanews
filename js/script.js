0b375cad917aa74f9abfc00679bd91fe4c2e1dda
Montana Sawyer <montanarsawyer@gmail.com>
2017-01-27 10:14:27 -0800

    began adding js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 $(function(){
-   var $menu = $('#sections-menu'),
-   $sectionMenu = $('.sections-button');
+    
+    //USE FILTER METHOD
 
-   $sectionMenu.click(function() {
-      $sectionMenu.toggleClass('active');
-      $menu.toggleClass('active');
-      return false;
-   });
+    $('#mySelect').change(function() {
+       var selectedSection = $(this).find('option:selected').val();
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
+        url += '?' + $.param({
+            'api-key': "49c6bed2e4044da2b7c4687f958f8164"
+        });
+        console.log(url);
+        $.ajax({
+            url: url,
+            method: 'GET',
+        })
+        .done(function(result) {
+            $('#search').toggleClass('resultsHeader');
+            $('#results').toggleClass('resultsSection');
+            var articleData = '';
+            console.log(result);
+            $.each(result.results, function( key, value ){
+                articleData += '<li>';
+                // articleData += '<img src="' + value.multimedia[4].url + '"/>';
+                articleData += '<p>' + value.abstract +'</p>'
+                articleData += '</li>';
+            });
+            $('.searchResults').append(articleData);
+        })
+
+        .fail(function(err) {
+            throw err;
+        });
+    });
 });
