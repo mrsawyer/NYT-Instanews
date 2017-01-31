71036aeffcfafaf632031b67b86eec58b41e8a99
Montana Sawyer <montanarsawyer@gmail.com>
2017-01-30 16:58:07 -0800

    add colour to button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(function(){
     
-    //USE FILTER METHOD
 
-    $('#mySelect').change(function() {
-       var selectedSection = $(this).find('option:selected').val();
+    $('#mySelect').change(function(e) {
+        e.preventDefault();
+        $('.searchResults').empty();
+        $('#results').prepend('<div class="load"><img src="../images/ajax-loader.gif"/></div>');
+        var selectedSection = $(this).find('option:selected').val();
         var url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
         url += '?' + $.param({
             'api-key': "49c6bed2e4044da2b7c4687f958f8164"
@@ -14,26 +16,34 @@ $(function(){
             method: 'GET',
         })
         .done(function(result) {
-            $('#search').toggleClass('resultsHeader');
-            $('#results').toggleClass('resultsSection');
+
+            $('#search').addClass('resultsHeader');
+            $('#results').addClass('resultsSection');
             var articleData = '';
             var articles = result.results;
             var articlesWithPics = articles.filter(function(options){
                     return options.multimedia.length !== 0;
                 })
-            console.log(articlesWithPics);
 
-            $.each(articlesWithPics, function( key, value ){
+            var topArticles = articlesWithPics.slice(0,12);
+
+            console.log(topArticles);
+
+            $.each(topArticles, function( key, value ){
                 articleData += '<li>';
-                articleData += '<img src="' + value.multimedia[4].url + '"/>';
-                articleData += '<p>' + value.abstract +'</p>'
+                articleData += '<div class="articleWrap" style="background-image: url('+value.multimedia[4].url+')">';
+                articleData += '<p class="abstract">' + value.abstract +'</p></div>'
                 articleData += '</li>';
             });
             $('.searchResults').append(articleData);
+
         })
 
         .fail(function(err) {
-            throw err;
-        });
+            $('.searchResults').append('No data to load');
+        })
+
+        .always(function(){ $('.load').remove();});
     });
+
 });
